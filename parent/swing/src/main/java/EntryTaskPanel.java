d77f7591e6e2b64284c8d479ba948cad2ad6d4da
Gabor Pinter <andzsinszan@gmail.com>
2016-01-27 17:14:07 +0900

    Entry Task: JPanel added

---
+++ parent/swing/src/main/java/EntryTaskPanel.java
@@ -0,0 +1,22 @@
+import info.pinlab.ttada.core.model.response.Response;
+import info.pinlab.ttada.core.model.response.ResponseContent;
+import info.pinlab.ttada.view.swing.AbstractTaskPanel;
+
+public class EntryTaskPanel extends AbstractTaskPanel{
+
+	@Override
+	public ResponseContent getResponse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setState(Response response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
